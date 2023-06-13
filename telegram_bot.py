82ef144ed0e26b7558e2f484d53e087469c0cd91
Wombatoff <mwombatoff@gmail.com>
2023-06-13 18:44:29 +0300

    add ConversationHandler

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,14 @@
 import logging
 import os
 import random
+from enum import Enum
+from functools import partial
 from logging.handlers import TimedRotatingFileHandler
 
 import redis
-
 from environs import Env
 from telegram import ReplyKeyboardMarkup
-from telegram.ext import Filters
+from telegram.ext import Filters, ConversationHandler, CommandHandler
 from telegram.ext import Updater, MessageHandler
 
 from create_intent import load_questions_answers
@@ -15,57 +16,77 @@ from create_intent import load_questions_answers
 bot_logger = logging.getLogger(__file__)
 
 
-def handle_message(update, context, questions_answers, redis_client):
-    message = update.message
-    chat_id = message.chat_id
-    text = message.text
+class BotState(Enum):
+    START = 1
+    NEW_QUESTION = 2
+    CHECK_ANSWER = 3
 
-    if text == 'Новый вопрос':
-        question = random.choice(list(questions_answers.keys()))
 
-        redis_client.set(chat_id, question)
+def handle_start(update, context):
 
+    custom_keyboard = [['Новый вопрос', 'Сдаться'],
+                       ['Мой счёт']]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Выберите вариант:",
+        reply_markup=reply_markup
+    )
+    return BotState.NEW_QUESTION
+
+
+def handle_new_question(update, context):
+    questions_answers = context.dispatcher.user_data['questions_answers']
+    redis_client = context.dispatcher.user_data['redis_client']
+
+    context.user_data['questions_answers'] = questions_answers
+    context.user_data['redis_client'] = redis_client
+
+    question = random.choice(list(questions_answers.keys()))
+
+    context.user_data['current_question'] = question
+
+    chat_id = update.message.chat_id
+    redis_client.set(f"question:{chat_id}", question)
+
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Вопрос: {question}"
+    )
+
+    return BotState.CHECK_ANSWER
+
+
+def handle_check_answer(update, context):
+    redis_client = context.dispatcher.user_data['redis_client']
+
+    chat_id = update.message.chat_id
+    current_question = redis_client.get(f"question:{chat_id}").decode('utf-8')
+
+    correct_answer = context.user_data['questions_answers'][current_question]
+    user_message = update.message.text
+    if "Ответ:" in user_message:
+        user_answer = user_message.split("Ответ:")[1].strip()
+    else:
+        user_answer = user_message
+
+    if user_answer.lower() == correct_answer.lower():
         context.bot.send_message(
-            chat_id=chat_id,
-            text=f'Вопрос: {question}'
+            chat_id=update.effective_chat.id,
+            text="Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»."
         )
-
-    elif text.startswith('Ответ:'):
-        user_answer = text.split(':')[1].strip()
-
-        previous_question_bytes = redis_client.get(chat_id)
-        if previous_question_bytes is not None:
-            previous_question = previous_question_bytes.decode()
-
-            correct_answer = questions_answers.get(previous_question)
-
-            if user_answer.lower() == correct_answer.lower():
-                context.bot.send_message(
-                    chat_id=chat_id,
-                    text='Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»'
-                )
-            else:
-                context.bot.send_message(
-                    chat_id=chat_id,
-                    text='Неправильно… Попробуешь ещё раз?'
-                )
-        else:
-            context.bot.send_message(
-                chat_id=chat_id,
-                text='Не найден предыдущий вопрос для проверки ответа.'
-            )
+        redis_client.incr(f"correct_answers:{chat_id}")
+        reply_markup = ReplyKeyboardMarkup([['Новый вопрос', 'Сдаться'], ['Мой счет']],
+                                           one_time_keyboard=True)
+        update.message.reply_text('Выберите действие:', reply_markup=reply_markup)
+        return BotState.NEW_QUESTION
 
     else:
-        custom_keyboard = [['Новый вопрос', 'Сдаться'],
-                           ['Мой счёт']]
-        reply_markup = ReplyKeyboardMarkup(custom_keyboard)
-
         context.bot.send_message(
-            chat_id=chat_id,
-            text='Custom Keyboard Test',
-            reply_markup=reply_markup
+            chat_id=update.effective_chat.id,
+            text="Неправильно... Попробуешь ещё раз?"
         )
-
+        return BotState.CHECK_ANSWER
 
 
 def main():
@@ -84,18 +105,28 @@ def main():
     env.read_env()
 
     telegram_token = env.str('TELEGRAM_TOKEN')
+    updater = Updater(token=telegram_token, use_context=True)
+
     quiz_files_folder = env.str('QUIZ_FILES_FOLDER')
     questions_answers = load_questions_answers(quiz_files_folder)
 
     redis_client = redis.Redis.from_url(env.str('REDIS_URL'))
 
+    updater.dispatcher.user_data['redis_client'] = redis_client
+    updater.dispatcher.user_data['questions_answers'] = questions_answers
 
-    updater = Updater(token=telegram_token, use_context=True)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(MessageHandler(
-        Filters.text & (~Filters.command),
-        lambda update, context: handle_message(update, context, questions_answers, redis_client)
-    ))
+    start_handler = CommandHandler('start', handle_start)
+
+    conversation_handler = ConversationHandler(
+        entry_points=[start_handler],
+        states={
+            BotState.START:  [MessageHandler(Filters.text & ~Filters.command, handle_start)],
+            BotState.NEW_QUESTION: [MessageHandler(Filters.text & ~Filters.command, handle_new_question)],
+            BotState.CHECK_ANSWER: [MessageHandler(Filters.text & ~Filters.command, handle_check_answer)],
+        },
+        fallbacks=[start_handler]
+    )
+    updater.dispatcher.add_handler(conversation_handler)
 
     updater.start_polling()
     updater.idle()
